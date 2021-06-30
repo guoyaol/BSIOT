34454cae4af7407a86482f23efc4fec02fd72f8d
Guoyao Li <1986876893@qq.com>
2021-06-30 23:57:39 +0800

    有了比较好看的索引页

--- fr/src/view/IndexView.js
+++ fr/src/view/IndexView.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Row, Col } from "antd";
+
+import { Card } from 'antd';
 
 import { NavLink } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb } from 'antd';
@@ -20,50 +23,82 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 class IndexView extends React.Component {
-    state = {
-        collapsed: false,
-      };
-    
-      onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({ collapsed });
-      };
 
-    
-      render() {
-        const { collapsed } = this.state;
-        return (
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-              <div className="logo" />
-              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<LikeOutlined />} onClick={() => {this.props.history.push("/device");}}>
-                  <LikeOutlined />    设备管理
-                </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />} onClick={() => {this.props.history.push("/message");}}>
-                <LikeOutlined />    消息查询
-                </Menu.Item>
-                <Menu.Item key="3" icon={<DesktopOutlined />} onClick={() => {this.props.history.push("/map");}}>
-                <LikeOutlined />    地图
-                </Menu.Item>
+  state = {
+    collapsed: false,
+  };
 
-              </Menu>
-            </Sider>
-            <Layout className="site-layout">
-              <Header className="site-layout-background" style={{ padding: 0 }} />
-              <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                  <Breadcrumb.Item>User</Breadcrumb.Item>
-                  <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                  Bill is a cat.
-                </div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-            </Layout>
-          </Layout>
-        );
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+
+  render() {
+    const count_device = 3;
+    const count_message = 129;
+    const { collapsed } = this.state;
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<LikeOutlined />} onClick={() => { this.props.history.push("/device"); }}>
+              <LikeOutlined />    设备管理
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />} onClick={() => { this.props.history.push("/message"); }}>
+              <LikeOutlined />    消息查询
+            </Menu.Item>
+            <Menu.Item key="3" icon={<DesktopOutlined />} onClick={() => { this.props.history.push("/map"); }}>
+              <LikeOutlined />    地图
+            </Menu.Item>
+
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              <div className="app-container">
+                <div className="site-card-wrapper">
+                  <Row gutter={16}>
+                    <Col span={8}>
+                      <Card title="设备总量" bordered={false}>
+                        {count_device}
+                      </Card>
+                    </Col>
+                    <Col span={8}>
+                      <Card title="消息总量" bordered={false}>
+                        {count_message}
+                      </Card>
+                    </Col>
+                    <Col span={8}>
+                      <Card title="登陆状态" bordered={false}>
+                        SUCCESS!
+                      </Card>
+                    </Col>
+                  </Row>
+                </div>,
+
+                <div><font size="15">每日消息数</font> </div>
+
+
+              </div>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>“设备已联网”物联网平台</Footer>
+        </Layout>
+
+
+
+      </Layout>
+
+
+    );
   }
 }
 
