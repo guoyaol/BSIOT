2084ff4fd3c5ea86ccbda9cb1d28ee7d663c885a
Guoyao Li <1986876893@qq.com>
2021-06-30 09:48:20 +0800

    删除了废旧的vue前端

--- frontend/src/api/test.js
+++
@@ -1,8 +0,0 @@
-import axios from '@/libs/api.request'
-
-export const getT = (query) => {
-  return axios.request({
-    url: '/Users/guoyaoli/Documents/BSIOT/test_api/a.json',
-    method: 'get'
-  })
-}
