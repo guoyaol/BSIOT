2cd06062ee9a6c8c5cc98e2d27d9f24922658e2a
Guoyao Li <1986876893@qq.com>
2021-07-03 00:16:43 +0800

    删除了旧版

--- submit_version/BSIOT/frontend/src/utils/history.js
+++
@@ -1,4 +0,0 @@
-import { createBrowserHistory } from 'history';
-
-/** 使用 history.push 进行路由跳转 **/
-export const history = createBrowserHistory();
--- submit_version/BSIOT/sql/user_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` VALUES (1, 'test', 'test', 'test@test.com');
-INSERT INTO `user` VALUES (2, 'abc', '$2a$10$xuYYXpzZD3MVuk1Zr3tR2ukndXptESDCQnFTKCrRLWpJpFpIItUBu', 'abc');
-INSERT INTO `user` VALUES (3, 'abc2', '$2a$10$scraT4.R5K26QjwQeqNIi.LH1YaK9bz6cyfk/0NQigTqSAD2Cdtfq', 'abc2');
-INSERT INTO `user` VALUES (4, 'abcd', '$2a$10$2G5QudWv1jmtNlRg/QDfte3Ja6QgQ9AJWW.L5WO2ViXFFuRf.m.jC', 'll');
